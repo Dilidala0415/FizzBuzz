9fe90ad0722ba69bf8aab5d23bd6018fd260f682
Yingeng Li <yingeng.li@mail.utoronto.ca>
2023-09-11 21:00:19 -0400

    refactored to extract helper method

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1;
+        int a = 0;
+        while (i < 1000) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                a += 1;
+            }
+            i += 1;
+        }
+        System.out.println(a);
+    }
 }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int s = 0;
+        while (n != 0) {
+            boolean divisibleBy2 = n % 2 == 0;
+            if (divisibleBy2) {
+                n = n/2;
+            }else {
+                n = n - 1;
+            }
+            s += 1;
+            System.out.println(s);
+        }
+    }
 }
